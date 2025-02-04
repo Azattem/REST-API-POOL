72cf0776d2a0d5329956ec373ba99fabe420ef41
azat <azatteam2013@yandex.ru>
2025-02-04 18:01:51 +0500

    Описал Client

--- src/API.java
+++ src/API.java
@@ -1,7 +1,28 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 public class API {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost",8080),0);
+        server.createContext("/api/v0/pool",(exchange -> {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String responseText = "Hello World! from our framework-less REST API\n";
+                exchange.sendResponseHeaders(200, responseText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(responseText.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            }
+            exchange.close();
+        }));
+        server.setExecutor(null);
+        server.start();
 
 
         System.out.println("Hello world!");
     }
+
 }
